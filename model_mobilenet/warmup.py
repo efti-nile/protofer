c666215e403986cf841dde7f0ae46d34e9ac261a
Efti Nile <efti.nile@gmail.com>
2024-04-22 21:47:07 +0300

    Create warmup.py

---
+++ model_mobilenet/warmup.py
@@ -0,0 +1,35 @@
+from tensorflow.keras.optimizers.legacy import Adam
+
+
+def warmup(model, ds, lr_list, loss, batch_size, metrics) -> None:
+
+    """
+    Warm up the model.
+    Args:
+        model (keras.model): Model
+        ds (tf.data.Dataset): Dataset
+        lr_list: List of learning rates
+        loss: Loss function, e.g. 'categorical_crossentropy'
+        batch_size: Batch size
+        metrics: Metrics, e.g. ['acc']
+    """
+
+    print("*** Warm up the Model")
+
+    for learning_rate in lr_list:
+
+        print(f"Using learning rate: {learning_rate}")
+
+        model.compile(
+            optimizer=Adam(learning_rate=learning_rate),
+            loss=loss,
+            metrics=metrics
+        )
+
+        model.fit(
+            x=ds,
+            y=None,
+            epochs=1,
+            batch_size=batch_size,
+            verbose=1
+        )
