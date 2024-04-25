18aaa7498350fa601dfc5ddcf119839c6bb54225
efti-nile <efti.nile@gmail.com>
2024-04-25 21:35:22 +0000

    modify plot_metrics

--- model_mobilenet/plot_metrics.py
+++ model_mobilenet/plot_metrics.py
@@ -14,16 +14,20 @@ def plot_metrics(history):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
     # Plot the training and validation loss.
-    ax1.plot(history['loss'], label="Training Loss")
-    ax1.plot(history['val_loss'], label="Validation Loss")
+    if history['loss']:
+        ax1.plot(history['loss'], label="Training Loss")
+    if history['val_loss']:
+        ax1.plot(history['val_loss'], label="Validation Loss")
     ax1.set_title("Loss")
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Loss")
     ax1.legend()
 
     # Plot the training and validation accuracy.
-    ax2.plot(history['accuracy'], label="Training Accuracy")
-    ax2.plot(history['val_accuracy'], label="Validation Accuracy")
+    if history['acc']:
+        ax2.plot(history['acc'], label="Training Accuracy")
+    if history['val_acc']:
+        ax2.plot(history['val_acc'], label="Validation Accuracy")
     ax2.set_title("Accuracy")
     ax2.set_xlabel("Epoch")
     ax2.set_ylabel("Accuracy")
