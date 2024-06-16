f1e8048777bef3bf126ecaec79e32e9ed6105e5e
Efti Nile <efti.nile@gmail.com>
2024-06-17 01:34:33 +0400

    wip: adding a processor task

--- model_mobilenet/image_server.py
+++ model_mobilenet/image_server.py
@@ -2,10 +2,30 @@ import asyncio
 import base64
 import cv2
 import random
+import tensorflow as tf
 import websockets
+
+from model import get_model
+
+
+classes = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+roi = [150, 150, 400, 400]
+
+
+def predict(model, image):
+    image = image[150:400, 150:400, :]
+    image = tf.image.resize(image, [48, 48])
+    image = tf.image.rgb_to_grayscale(image)
+    image = tr.cast(image, tf.float32) / 255.0
+    batch = tf.expand_dims(image, axis=0)
+    scores = model(batch)
+    class_id = tf.math.argmax(scores)
+    class_name = classes[class_id]
+    return class_name
 
 
 async def producer(queue: asyncio.Queue, frames_to_skip=20):
+    model = get_model()
     cap = await asyncio.to_thread(cv2.VideoCapture, 0)
     if not cap.isOpened():
         raise RuntimeError("video capture is not open")
@@ -15,11 +35,17 @@ async def producer(queue: asyncio.Queue, frames_to_skip=20):
         for _ in range(frames_to_skip):
             await asyncio.to_thread(cap.grab)
         ret, frame = await asyncio.to_thread(cap.read)
+        # prediction = await asyncio.to_thread(predict, model, frame)
+        # prediction = predict(model, frame)
         if not ret:
             raise RuntimeError("frame is not returned")
         _, image_buffer = cv2.imencode(".jpg", frame)
         encoded_image = base64.b64encode(image_buffer).decode("utf-8")
         await queue.put(encoded_image)
+        # await queue.put({
+        #     'image': encoded_image,
+        #     'prediction': prediction
+        # })
         print("frame is put")
 
 
